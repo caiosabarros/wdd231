15f76f09eb27be8950da0e4ecfc60ec295be1170
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2025-01-08 19:31:54 -0300

    add waywind

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,17 @@ hamButton.addEventListener("click", () => {
     navigationMenu.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+// waywinding
+function wayWind() {
+    const currentPagePath = window.location.pathname;
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach((navLink) => {
+        if (String(navLink.getAttribute("href")).includes(navLink.textContent)) {
+            // TODO: remove it from HOME nav link
+            navLink.classList.add("active");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", wayWind);
